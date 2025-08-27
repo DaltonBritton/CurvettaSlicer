8d5e303624dd1af8d7fb42623d9c52b6eb1491d6
DaltonBritton <157634547+DaltonBritton@users.noreply.github.com>
2025-08-26 21:48:02 -0600

    Marked functions with nodiscard libslic3r/Arachne/LimitedBeadingStrategy.cpp

--- src/libslic3r/Arachne/BeadingStrategy/LimitedBeadingStrategy.hpp
+++ src/libslic3r/Arachne/BeadingStrategy/LimitedBeadingStrategy.hpp
@@ -33,15 +33,15 @@ public:
     
     ~LimitedBeadingStrategy() override = default;
     
-    Beading compute(coord_t thickness, coord_t bead_count) const override;
-    coord_t getOptimalThickness(coord_t bead_count) const override;
-    coord_t getTransitionThickness(coord_t lower_bead_count) const override;
-    coord_t getOptimalBeadCount(coord_t thickness) const override;
-    std::string toString() const override;
+    [[nodiscard]] Beading compute(coord_t thickness, coord_t bead_count) const override;
+    [[nodiscard]] coord_t getOptimalThickness(coord_t bead_count) const override;
+    [[nodiscard]] coord_t getTransitionThickness(coord_t lower_bead_count) const override;
+    [[nodiscard]] coord_t getOptimalBeadCount(coord_t thickness) const override;
+    [[nodiscard]] std::string toString() const override;
     
-    coord_t getTransitioningLength(coord_t lower_bead_count) const override;
+    [[nodiscard]] coord_t getTransitioningLength(coord_t lower_bead_count) const override;
     
-    float getTransitionAnchorPos(coord_t lower_bead_count) const override;
+    [[nodiscard]] float getTransitionAnchorPos(coord_t lower_bead_count) const override;
 
 protected:
     const coord_t max_bead_count;
